607b63711df6c285d811e74374c925c588c0d0c0
Thomas Moraes <thomas.moraes@semexe.com>
2022-05-14 11:45:06 -0300

    Padrao Template Method

---
+++ src/Impostos/Icpp.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Alura\DesignPatterns\Impostos;
+
+use Alura\DesignPatterns\Orcamento;
+
+class Icpp extends ImpostoCom2Aliquotas
+{
+    protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
+    {
+        return $orcamento->valor > 500;
+    }
+
+    protected function calculaTaxaMaxima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.03;;
+    }
+
+    protected function calculaTaxaMinima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.02;
+    }
+}
---
+++ src/Impostos/Ikcv.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Alura\DesignPatterns\Impostos;
+
+use Alura\DesignPatterns\Orcamento;
+
+class Ikcsv extends ImpostoCom2Aliquotas
+{
+    protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
+    {
+        return $orcamento->valor > 300 && $orcamento->quantidadeItens > 3;
+    }
+
+    protected function calculaTaxaMaxima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.04;;
+    }
+
+    protected function calculaTaxaMinima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.025;
+    }
+}
